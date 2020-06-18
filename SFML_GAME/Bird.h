68ef9d7352e6aca88380c63250290cd85195a2ce
Sara <saraabuhattab99@gmail.com>
2020-06-17 20:01:59 -0400

    Final Copy

---
+++ SFML_GAME/Bird.h
@@ -0,0 +1,46 @@
+#pragma once
+
+#include <SFML/Graphics.hpp>
+
+#include "DEFINITIONS.h"
+#include "Game.h"
+
+#include <vector>
+
+namespace Sonar
+{
+	class Bird
+	{
+	public:
+		Bird(GameDataRef data);
+		~Bird();
+
+		void Draw();
+
+		void Animate(float dt);
+
+		void Update(float dt);
+
+		void Tap();
+
+		const sf::Sprite& GetSprite() const;
+
+	private:
+		GameDataRef _data;
+
+		sf::Sprite _birdSprite;
+		std::vector<sf::Texture> _animationFrames;
+
+		unsigned int _animationIterator;
+
+		sf::Clock _clock;
+
+		sf::Clock _movementClock;
+
+		int _birdState;
+
+		float _rotation;
+
+	};
+}
+
